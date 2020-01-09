30979252ab310c7f4f43e34c8d424b630f5169e6
Kalpanab87 <kalpana7.ece@gmail.com>
2020-01-09 10:37:25 +0000

    Using Random Instance and package Imports

---
+++ src/learnProgramming/ImportExample.java
@@ -0,0 +1,14 @@
+package learnProgramming;
+
+import a.b.MyClass;
+
+import java.util.Random;
+
+public class ImportExample {
+    public static void main(String[] args) {
+
+        Random r= new Random();
+        MyClass myClassobject = new MyClass();
+        System.out.println(r.nextInt(100));
+    }
+}
